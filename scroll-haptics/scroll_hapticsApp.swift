53a2c1306631f86f77a2f020cf7e3bb4d44cc2e0
Prashant Garg <prashant.garg.1901@gmail.com>
2024-06-27 22:51:17 +0530

    Initial Commit

---
+++ scroll-haptics/scroll_hapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  scroll_hapticsApp.swift
+//  scroll-haptics
+//
+//  Created by Prashant Garg on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct scroll_hapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
